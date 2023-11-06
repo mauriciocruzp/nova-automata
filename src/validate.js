ea79ce2ba2ac4745258b3003501d47d2dace8936
Mauricio Cruz <mawcp7@gmail.com>
2023-11-06 17:52:02 -0600

    refactor validation rules in validate.js

--- src/validate.js
+++ src/validate.js
@@ -1,18 +1,19 @@
 export const validate = (input) => {
   input = input.replace(/\s+/g, '');
 
-  const splitInput = input.split(/(while|cycle|when|fn|_)/);
+  const splitInput = input.split(/(while|cycle|when|fn|_|run)/);
   let result = [];
   for (let i = 1; i < splitInput.length; i += 2) {
     result.push(splitInput[i] + (splitInput[i + 1] || ''));
 }
 
   const grammar = [
-    /^_(\w+):((int|boolean|string|float)|(int=[0-9]+|boolean=(true|false)|string:["'][^"']*["']|float:\d+(\.\d+)));$/,
-    /^while\((((\w+)|[0-9]+)(<|>|<=|>=|==|!=)((\w+)|[0-9]+)|(true|false))\){INST}$/,
+    /^_(\w+):((int|boolean|string|float)|(int=([0-9]+|array.length)|boolean=(true|false)|string=["'][^"']*["']|float=\d+(\.\d+)));$/,
+    /^while\((\w+)(<|>|<=|>=|==|!=)(\w+)\){INST}$/,
     /^cycle\(i:int=[0-9]+;i(<|>|<=|>=|==|!=)(\w+);i(\+\+|--)\){INST}$/,
-    /^(when\((\w+)(<|>|<=|>=|==|!=)(\w+)\){INST}|when\((\w+)(<|>|<=|>=|==|!=)(\w+)\){INST}so{INST})$/,
-    /^fn(\w+)\(((\w+)(int|boolean|string|float)+|)\)(int{return([0-9]+|(\w+)|null)|boolean{return(true|false|(\w+)|null)|string{return(["'][^"']*["']|(\w+)|null)|float{return (\d+(\.\d+)|(\w+)|null));}$/
+    /^(when\(((\w+)|[0-9]+)(<|>|<=|>=|==|!=)((\w+)|[0-9]+|true|false)\){INST}|when\((\w+)(<|>|<=|>=|==|!=)(\w+)\){INST}so{INST})$/,
+    /^fn(\w+)\(((\w+)(int|boolean|string|float)|(\w+)(int|boolean|string|float),(\w+)(int|boolean|string|float))\)(int{return([0-9]+|(\w+)|null)|boolean{return(true|false|(\w+)|null)|string{return(["'][^"']*["']|(\w+)|null)|float{return(\d+(\.\d+)|(\w+)|null));}$/,
+    /^run\(\){INST}$/
   ]
 
   console.log(input)
@@ -23,6 +24,7 @@ export const validate = (input) => {
       return {success: false, msg: `Error en el bloque de codigo ${result[i]}`};
     }
   }
+
 
   return {success: true, msg: 'Codigo valido'};
 }
